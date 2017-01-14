6b97b00ecbafe9d2868c451326e82dd6702dba1d
Carsten Schoenert <c.schoenert@t-online.de>
2017-01-14 11:12:30 +0100

    tests.sh: improve script output

--- debian/tests/tests.sh
+++ debian/tests/tests.sh
@@ -1,33 +1,57 @@
 #!/bin/sh
 
 set -e
-set -x
+#set -x
 
 cd debian/tests
 
-echo "Compiling, linking the CoAP testing server ..."
+echo -n "Compiling, linking the CoAP testing server using C source ... "
 gcc -g -o coap-test-server coap-test-server.c -O2 -D_GNU_SOURCE -DWITH_POSIX `pkg-config --libs libcoap-1`
-[ "$?" != "0" ] && echo "Failed!" && exit 1
+if [ "$?" != "0" ]; then
+    echo "Failed!"
+    exit 1
+else
+    echo "Successful!"
+fi
 
-echo "Starting the CoAP testing server ..."
+echo -n "Starting the CoAP testing server ... "
 ./coap-test-server &
 
 PID_COAP_SERVER=$(pidof coap-test-server)
-[ "${PID_COAP_SERVER}" = "" ] && echo "Failed!" && exit 1
+if [ "${PID_COAP_SERVER}" = "" ]; then
+    echo "Failed!"
+    exit 1
+else
+    echo "Successful!"
+fi
 
-echo "Probing CoAP testing server using IPv4 ..."
+echo -n  "Probing CoAP testing server using IPv4 ... "
 RET=$(coap-client -m get coap://127.0.0.1/ | grep CoAP)
 
-[ "${RET}" = "" ] && echo "Failed!" && exit 1
+if [ "${RET}" = "" ]; then
+    echo "Failed!"
+    exit 1
+else
+    echo "Successful!"
+fi
+
 RET=""
 
-echo "Probing CoAP testing server using IPv6 ..."
+echo -n "Probing CoAP testing server using IPv6 ... "
 RET=$(coap-client -m get coap://[::1]/ | grep CoAP)
 
-[ "${RET}" = "" ] && echo "Failed!" && exit 1
+if [ "${RET}" = "" ]; then
+    echo "Failed!"
+    exit 1
+else
+    echo "Successful!"
+fi
+
 RET=""
 
 kill ${PID_COAP_SERVER}
 
-echo 'Testing coap-test-server succesful!'
+echo "[HINT] Compiling, linking the CoAP testing server using C++ source is still needed to add here."
+
+echo 'Testing coap-test-server was successful!'
 exit 0
